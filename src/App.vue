b981593742c8341a69b6c152a3a8c6e5c4972973
Asarua <1299458195@qq.com>
2021-03-03 09:12:13 +0800

    feat(all): 新建Demo自调用组件

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <demo :info="info" />
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo'
+export default {
+  data() {
+    return {
+      info: [
+        {
+          a: 1,
+          b: 2,
+          c: [
+            {
+              a: 11,
+              c: {
+                a: 111
+              }
+            }
+          ]
+        },
+        {
+          a: 3,
+          b: 4
+        }
+      ]
+    }
+  },
+  components: {
+    Demo
+  }
+}
+</script>
---
+++ src/components/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="(item, index) in getArray" :key="index">
+      <el-button>
+        {{item.a}}
+      </el-button>
+      <div v-if="!!item.c">
+        <demo :info="item.c" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo',
+  props: {
+    info: {
+      type: [Object, Array]
+    }
+  },
+  computed: {
+    getArray() {
+      const { info } = this
+      return Array.isArray(info) ? info : [info]
+    }
+  },
+}
+</script>
